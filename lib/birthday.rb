e22694c66b2122d41955ad79b2e51d12a71bf93b
Megan (AKA Pilahi) Moran <kamokumoran@gmail.com>
2020-08-01 23:08:19 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,8 +10,11 @@ def happy_birthday(birthday_kids)
   end
 end
 
-def happy_birthday(birthday_kids)
+def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    if age? <= 12
+      puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+    else 
+      puts 
   end
 end
